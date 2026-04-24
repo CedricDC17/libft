23227fd788eace0beea6415027514778bef74724
CedricDC17 <cedric.dc91@gmail.com>
2026-04-24 17:57:02 +0200

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -23,7 +23,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 		return (ft_strlen(src));
 	}
 	i = 0;
-	while (i < size - 1)
+	while (i < size - 1 && src[i])
 	{
 		dst[i] = src[i];
 		i++;
